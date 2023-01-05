6ef1d81c6939af1263984f25da8d12e420e2e9e3
dudekmat <matdudek2706@gmail.com>
2023-01-05 19:14:24 +0100

    Added login

--- src/app/auth/shared/auth.service.ts
+++ src/app/auth/shared/auth.service.ts
@@ -2,7 +2,10 @@ import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import { HttpClient } from '@angular/common/http';
 import { SignupRequestPayload } from '../signup/signup-request.payload';
-import { Observable } from 'rxjs';
+import { map, Observable } from 'rxjs';
+import { LoginRequestPayload } from '../login/login-request.payload';
+import { LoginResponse } from '../login/login-response.payload';
+import { LocalStorageService } from 'ngx-webstorage';
 
 @Injectable({
   providedIn: 'root'
@@ -11,9 +14,20 @@ export class AuthService {
 
   private baseUrl = `${environment.apiUrl}/auth`;
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private localStorage: LocalStorageService) { }
 
   signup(signupRequestPayload: SignupRequestPayload): Observable<any> {
     return this.http.post(`${this.baseUrl}/signup`, signupRequestPayload)
+  }
+
+  login(LoginRequestPayload: LoginRequestPayload): Observable<boolean> {
+    return this.http.post<LoginResponse>(`${this.baseUrl}/login`, LoginRequestPayload)
+      .pipe(map(data => {
+        this.localStorage.store('authenticationToken', data.authenticationToken);
+        this.localStorage.store('username', data.username);
+        this.localStorage.store('refreshToken', data.refreshToken);
+        this.localStorage.store('expiresAt', data.expiresAt);
+        return true;
+      }))
   }
 }
