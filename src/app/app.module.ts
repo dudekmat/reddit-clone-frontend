b2c3e9567e94449a530f337f922e955bd31459d1
dudekmat <matdudek2706@gmail.com>
2023-01-08 18:15:04 +0100

    Added subreddit and post components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,12 +12,28 @@ import { ToastrModule } from 'ngx-toastr';
 import { TokenInterceptor } from './shared/token-interceptor';
 import { HomeComponent } from './home/home.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { PostTileComponent } from './shared/post-tile/post-tile.component';
+import { SideBarComponent } from './shared/side-bar/side-bar.component';
+import { SubredditSideBarComponent } from './shared/subreddit-side-bar/subreddit-side-bar.component';
+import { VoteButtonComponent } from './shared/vote-button/vote-button.component';
+import { CreateSubredditComponent } from './subreddit/create-subreddit/create-subreddit.component';
+import { CreatePostComponent } from './post/create-post/create-post.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { ListSubredditsComponent } from './subreddit/list-subreddits/list-subreddits.component';
+import { EditorModule } from '@tinymce/tinymce-angular';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    HomeComponent
+    HomeComponent,
+    PostTileComponent,
+    SideBarComponent,
+    SubredditSideBarComponent,
+    VoteButtonComponent,
+    CreateSubredditComponent,
+    CreatePostComponent,
+    ListSubredditsComponent
   ],
   imports: [
     BrowserModule,
@@ -27,7 +43,9 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
     NgxWebstorageModule.forRoot(),
     BrowserAnimationsModule,
     ToastrModule.forRoot(),
-    FontAwesomeModule
+    FontAwesomeModule,
+    ReactiveFormsModule,
+    EditorModule
   ],
   providers: [
     {
