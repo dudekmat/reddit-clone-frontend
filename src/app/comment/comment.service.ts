2efa2eb688b432b8deba95aa7f56947300edafef
dudekmat <matdudek2706@gmail.com>
2023-01-10 23:59:15 +0100

    Added comments section for post

---
+++ src/app/comment/comment.service.ts
@@ -0,0 +1,23 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { CommentModel } from './comment.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CommentService {
+
+  private baseUrl = `${environment.apiUrl}/comments`;
+
+  constructor(private http: HttpClient) { }
+
+  getAllCommentsForPost(postId: number): Observable<Array<CommentModel>> {
+    return this.http.get<Array<CommentModel>>(`${this.baseUrl}/by-post/${postId}`);
+  }
+
+  postComment(commentPayload: CommentModel): Observable<any> {
+    return this.http.post<any>(this.baseUrl, commentPayload);
+  }
+}
--- src/app/post/view-post/view-post.component.ts
+++ src/app/post/view-post/view-post.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import { throwError } from 'rxjs';
+import { CommentModel } from 'src/app/comment/comment.model';
+import { CommentService } from 'src/app/comment/comment.service';
 import { PostModel } from 'src/app/shared/post.model';
 import { PostService } from 'src/app/shared/post.service';
 
@@ -13,14 +16,54 @@ export class ViewPostComponent implements OnInit {
 
   postId: number;
   post: PostModel;
+  commentForm: FormGroup;
+  commentPayload: CommentModel;
+  comments: Array<CommentModel> = [];
 
-  constructor(private postService: PostService, private activatedRoute: ActivatedRoute) { }
+  constructor(private postService: PostService, private activatedRoute: ActivatedRoute,
+    private commentService: CommentService) { }
 
   ngOnInit(): void {
     this.postId = this.activatedRoute.snapshot.params['id'];
+    this.getPostById(this.postId);
+    this.getCommentsForPost();
+
+    this.commentForm = new FormGroup({
+      text: new FormControl('', Validators.required)
+    });
+
+    this.commentPayload = {
+      text: '',
+      postId: this.postId
+    }
+  }
+
+  postComment() {
+    this.commentPayload.text = this.commentForm.get('text').value;
+    this.commentService.postComment(this.commentPayload).subscribe(
+      data => {
+        this.commentForm.get('text').setValue('');
+        this.getCommentsForPost();
+      }, error => {
+        throwError(error);
+      }
+    );
+  }
+
+  private getPostById(id: number) {
     this.postService.getPost(this.postId).subscribe(
       data => {
         this.post = data;
+      }, error => {
+        throwError(error);
+      }
+    );
+  }
+
+  private getCommentsForPost() {
+    this.commentService.getAllCommentsForPost(this.postId).subscribe(
+      data => {
+        this.comments = data;
       }, error => {
         throwError(error);
       }
